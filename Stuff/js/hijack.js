c92f3f0e9e50500148811823b6db23859fb9f542
Nomulous <martin.ctuzim@gmail.com>
2017-11-12 16:42:35 -0500

    actual prod

--- Stuff/js/hijack.js
+++ Stuff/js/hijack.js
@@ -3,16 +3,32 @@ var loaded2 = false;
 var safewebsite = "paypal.com";
 var safewebsite2 = "paypal.me";
 var audio = new Audio('sound/nope.mp3');
+var active = true;
+
+
+var setactive=function(nvar) {
+  active = nvar;
+}
+
+var getactive=function() {
+  return active;
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  //chrome.tabs.executeScript(tab.id, {code:"alert()"});
+});
 
 chrome.webNavigation.onCompleted.addListener(function (details) {
+  if(active){
   console.log(details);
   if(details.url != "http://martintuzim.com/saved.html") {
   console.log("Website loaded!");
   loaded = true;
-} else { loaded = false;}
+} else { loaded = false;}}
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)  {
+if(active){
  console.log("l1:" + loaded + ",l2:" + loaded2);
   if(loaded && loaded2 && !tab.url.includes(safewebsite) && !tab.url.includes(safewebsite2)){
   console.log(tabId);
@@ -32,5 +48,5 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)  {
 }
 if(loaded){
   loaded2 = true;
-}
+}}
 });
