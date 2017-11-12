ca89121181be4bd794e3a1a84cb01ff572a72bb0
Nuliflyer <tyler@hyperweb.ca>
2017-11-12 14:34:43 -0500

    Working, but only kinda
    works, but not how we want, option other than windows.onload is preferable here.

--- Stuff/hijack.js
+++ Stuff/hijack.js
@@ -1,5 +1,7 @@
 chrome.webNavigation.onBeforeNavigate.addListener(
   function(callback){
   console.log(callback.url);
-
+  chrome.tabs.executeScript({
+    code: 'window.onload = function() {	location.href = "http://martintuzim.com/saved.html";}'
+  });
 });
