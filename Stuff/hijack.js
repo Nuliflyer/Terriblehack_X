897ba82ec7442ecdb89f78da8ab086a8eb8e6449
Nuliflyer <tyler@hyperweb.ca>
2017-11-12 13:54:51 -0500

    Back to working version that doesn't do what we want

--- Stuff/hijack.js
+++ Stuff/hijack.js
@@ -1,3 +1,10 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function(callback){
-  console.log("IT WORKS");
+chrome.webNavigation.onBeforeNavigate.addListener(
+  function(callback){
+  console.log(callback.url);
+
+//  var rule = {
+//    conditions: []
+//    actions: [window.location="http://www.tutorialspoint.com"]
+//  }
+
 });
