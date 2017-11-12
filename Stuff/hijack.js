8f1b856b2416d626687f9242463682bd30f9cd6b
Nuliflyer <tyler@hyperweb.ca>
2017-11-12 13:58:40 -0500

    deleted useless comments

--- Stuff/hijack.js
+++ Stuff/hijack.js
@@ -2,9 +2,4 @@ chrome.webNavigation.onBeforeNavigate.addListener(
   function(callback){
   console.log(callback.url);
 
-//  var rule = {
-//    conditions: []
-//    actions: [window.location="http://www.tutorialspoint.com"]
-//  }
-
 });
