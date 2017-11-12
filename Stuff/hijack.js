da01e2542207c00c1a9063e53bde8f2c18c23b83
Nomulous <martin.ctuzim@gmail.com>
2017-11-12 15:51:49 -0500

    finished

--- Stuff/hijack.js
+++ Stuff/hijack.js
@@ -1,10 +1,33 @@
-chrome.webNavigation.onBeforeNavigate.addListener(
-  function(callback){
-  console.log(callback.url);
+var loaded = false;
+var loaded2 = false;
+var safewebsite = "paypal.com";
+var safewebsite2 = "paypal.me";
+chrome.webNavigation.onCompleted.addListener(function (details) {
+  console.log(details);
+  if(details.url != "http://martintuzim.com/saved.html") {
+  console.log("Website loaded!");
+  loaded = true;
+} else { loaded = false;}
+});
 
-//  var rule = {
-//    conditions: []
-//    actions: [window.location="http://www.tutorialspoint.com"]
-//  }
-
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)  {
+ console.log("l1:" + loaded + ",l2:" + loaded2);
+  if(loaded && loaded2 && !tab.url.includes(safewebsite) && !tab.url.includes(safewebsite2)){
+  console.log(tabId);
+  try{
+  var myNewUrl = "http://martintuzim.com/saved.html";
+  console.log(tab);
+  if(tab.url != myNewUrl && loaded) {
+    loaded = false;
+    loaded2 = false;
+    console.log("Not on saved site, switch to it!");
+    chrome.tabs.update(tabId, {url: myNewUrl});
+}
+} catch (e) {
+  console.log("Err:" + e);
+}
+}
+if(loaded){
+  loaded2 = true;
+}
 });
